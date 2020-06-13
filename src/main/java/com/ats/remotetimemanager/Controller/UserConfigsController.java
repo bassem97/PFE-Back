f1272bcf0aede2c0afbe44d5cada26911c1510ca
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-06-13 08:45:47 +0100

    h

--- src/main/java/com/ats/remotetimemanager/Controller/UserConfigsController.java
+++ src/main/java/com/ats/remotetimemanager/Controller/UserConfigsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/userConfigs/")
+@RequestMapping("/userConfigs   /")
 public class UserConfigsController {
 
     @Autowired
