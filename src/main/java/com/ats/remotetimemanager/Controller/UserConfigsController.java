fefadb43b689a523c457edbe78e7c3d1425d8527
bassem jadoui <36534106+bassem97@users.noreply.github.com>
2020-06-13 08:45:04 +0100

    userConfigs fini

--- src/main/java/com/ats/remotetimemanager/Controller/UserConfigsController.java
+++ src/main/java/com/ats/remotetimemanager/Controller/UserConfigsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/userConfig/")
+@RequestMapping("/userConfigs/")
 public class UserConfigsController {
 
     @Autowired
