04f9ddc2309d70885492612d5435a82c94eaadf3
ReserveHotelOnline <dr.kira1@hotmail.com>
2020-06-25 00:17:42 +0100

    notif + post

--- src/main/java/com/ats/remotetimemanager/Controller/DBAutoBackupController.java
+++ src/main/java/com/ats/remotetimemanager/Controller/DBAutoBackupController.java
@@ -1,38 +1,62 @@
 package com.ats.remotetimemanager.Controller;
 
+import com.ats.remotetimemanager.Model.Department;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/db/")
 @Configuration
 @EnableScheduling
 public class DBAutoBackupController {
     private final Path backUp = Paths.get("BackUp");
     private final Path dataBase = Paths.get("Database");
-//    @Scheduled(cron = "0 30 1 * * ?")
-    @Scheduled(cron = "*/15 * * * * *")
+    private Long custom = null;
+
+    @RequestMapping("dumpDb/{custom}")
+    public void customExport(@PathVariable("custom") Long cs) {
+        custom = cs;
+        schedule();
+    }
+//    @Scheduled(cron = "*/30 * * * * *") // every 30 seconds
+    @Scheduled(cron = "0 0 23 * * *") // everyday at 23h
+
     public void schedule() {
         System.out.println("Backup Started at " + new Date());
 
         Date backupDate = new Date();
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
         String backupDateStr = format.format(backupDate);
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH_mm_ss");
+        String time = timeFormat.format(backupDate);
         String dbName = "remotetimemanager";
         String dbUserName = "root";
         Path mysqldump =this.dataBase.resolve("mysqldump");
-
-        String folderPath = "C:\\Users\\Bassem's PC\\Desktop\\PFE\\PFE-Back\\BackUp";
+        String folderPath;
+        if (custom == null) {
+            folderPath  = "C:\\Users\\khail\\Desktop\\PFE\\PFE-Back\\DailyBackUp";
+        } else {
+            folderPath  = "C:\\Users\\khail\\Desktop\\PFE\\PFE-Back\\CustomBackUp";
+        }
         File f1 = new File(folderPath);
         f1.mkdir();
-        String fileName = "Daily_DB_Backup_"+backupDateStr+".sql";
+        String fileName;
+        if (custom == null) {
+            fileName  = "Daily_DB_Backup_" + backupDateStr + ".sql";
+        } else {
+            fileName = "Custom_DB_Backup"+backupDateStr+"_"+time+".sql";
+        }
+        custom = null;
         Path backup = this.backUp.resolve(fileName);
 
         String executeCmd = mysqldump+ " --no-create-db --no-create-info -u "+dbUserName+" "+dbName+" -r "+ backup;
@@ -55,4 +79,5 @@ public class DBAutoBackupController {
             System.out.println("Backup Failure");
         }
     }
+
 }
