a98bec233faf5434bc446358757a89407b18500b
bassem jadoui <36534106+bassem97@users.noreply.github.com>
2020-06-27 02:12:08 +0100

    database backup  nin

--- rollback.bat
+++ rollback.bat
@@ -1,1 +1,1 @@
-Database\mysql -u root remotetimemanager < Backup\DailyBackup\Daily_DB_Backup_25-06-2020.sql
+Database\mysql -u root remotetimemanager < Backup\DailyBackup\Daily_DB_Backup_26-06-2020.sql
